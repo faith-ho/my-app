257fdd3e0f15912ed41b82ce6482b44e94552be9
Faith Ho <faithho@deloitte.co.uk>
2022-04-27 17:48:47 +0100

    add styled components and refactoring

--- src/Components/CVList.js
+++ src/Components/CVList.js
@@ -1,15 +1,39 @@
 import React from "react";
+import { StyledCVSection } from "../Styles/CVSection.styled";
 
-export const Education = (
-  <div>
-    <h3>Education Heading</h3>
-    <body>Education Content </body>
-  </div>
-);
+export default function CVSection({
+  chosenSection,
+  sectionHeading,
+  role,
+  location,
+  description,
+}) {
+  if (chosenSection === "Education") {
+    sectionHeading = "Education Section";
+    role = "MSc International Political Economy";
+    location = "London School of Economics";
+    description =
+      "Frequently assessed essay-writing, weekly presentations with Q&As, weekly research memos, two-team policy debates, completing problem-sets that compared 2-3 economic policy papers, and using R and STATA to conduct data analysis.";
+  }
 
-export const workExperience = (
-  <div>
-    <h3>Work Experience Heading</h3>
-    <body>Work Experience Content </body>
-  </div>
-);
+  if (chosenSection === "Work Experience") {
+    sectionHeading = "Work Experience Heading";
+    role = "Work Experience Role";
+    description = "Work Experience Description";
+  }
+
+  if (chosenSection === "Extra-Curricular Experience") {
+    sectionHeading = "Extra-Curricular Experience";
+    role = "Extra-Curricular Role";
+    description = "Extra-Curricular Description";
+  }
+
+  return (
+    <StyledCVSection>
+      <h2>{sectionHeading}</h2>
+      <h3>{role}</h3>
+      <h4>{location}</h4>
+      <body>• {description}</body>
+    </StyledCVSection>
+  );
+}
---
+++ src/Styles/CVSection.styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledCVSection = styled.div`
+  color: black;
+
+  h2 {
+    background-color: #00a3cc;
+    font-style: oblique;
+  }
+
+  h3 {
+    background-color: #1ad1ff;
+    font-style: italic;
+  }
+
+  h4 {
+    background-color: #66e0ff;
+    font-style: italic;
+  }
+
+  body {
+    background-color: #ccf5ff;
+    font-style: normal;
+  }
+
+  &:hover {
+    color: white;
+  }
+`;
